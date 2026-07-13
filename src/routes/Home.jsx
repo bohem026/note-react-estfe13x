fabd2a16dfe3ca51d9192459766d943e142c20d6
bohem dev <microbin98@gmail.com>
2026-07-13 13:31:56 +0900

    Refactor App and Router components to manage user authentication state and pass userId; implement Comment component for better comment display and management

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -22,8 +22,9 @@ import {
 } from 'firebase/firestore';
 import { db } from '../firebase';
 import { useEffect, useState } from 'react';
+import Comment from '../components/Comment';
 
-function Home() {
+function Home({ userId }) {
   const [comment, setComment] = useState('');
   const [comments, setComments] = useState([]);
   /*
@@ -56,6 +57,7 @@ function Home() {
         // comment: comment,
         comment,
         date: serverTimestamp(),
+        uid: userId,
       });
       setComment('');
       // getComments();
@@ -87,14 +89,9 @@ function Home() {
         </Button>
       </Box>
       <Divider sx={{ my: 3 }} />
-      <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+      <List sx={{ width: '100%' }}>
         {comments.map((item) => (
-          <ListItem key={item.id} alignItems="flex-start" divider>
-            <ListItemText
-              primary={item.comment}
-              secondary={item.date?.toDate() ? item.date.toDate().toLocaleString() : '작성 시간 없음'}
-            />
-          </ListItem>
+          <Comment key={item.id} item={item} isShown={userId === item.uid} />
         ))}
       </List>
     </>
